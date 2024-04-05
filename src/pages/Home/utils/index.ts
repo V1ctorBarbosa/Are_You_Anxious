91af5a095e87ba77907c72b537e1575095c08deb
Victor <vicfer2708@gmail.com>
2024-04-05 16:43:29 -0300

    feat: steps definition and data

--- src/pages/Home/utils/index.ts
+++ src/pages/Home/utils/index.ts
@@ -1,15 +1,39 @@
 export const questionsPath = {
-    step1: {
-        id: 1,
-        info: "are you anxious?",
-        confirm: "yeah...",
-        deny: "nah... i'm good"
-    },
-    step2: {
-        id: 2,
-        info: "that's really good to know, but i'm kinda trying to make something for the depressed people out there, so...",
-        confirm: "ok... let's do try this again",
-        deny: "so get me out of here",
-        denyLink: ""
-    }
-}
+  step1: {
+    id: 1,
+    info: "are you anxious?",
+    confirm: "yeah...",
+    deny: "nah... i'm good",
+  },
+  step2: {
+    id: 2,
+    info: "that's really good to know, but i'm kinda trying to make something for the depressed people out there, so...",
+    confirm: "ok... let's do try this again",
+    deny: "so get me out of here",
+    link: "https://www.tiktok.com/",
+  },
+  step3: {
+    id: 3,
+    info: "I understand... I often feel this way too. Did something happen?",
+    confirm: "something just happened...",
+    deny: "It's that usual anxiety",
+  },
+  step4: {
+    id: 4,
+    info: "But I know, I read that Buddhists believe that true happiness lies in ‘being in the present’. I think I almost always feel anxious, it's because I'm paying attention to everything, except that it's here and now",
+    confirm: "i don't get it",
+    deny: "make sense...",
+  },
+  step5: {
+    id: 5,
+    info: "I want to share something with you that talks a little about this. When I'm at my worst, it makes me feel a little better about things Maybe just like it is with me, it will make you feel a little better too :)",
+    confirm: "ok... let's do try this again",
+    deny: "so get me out of here",
+    link: "https://www.youtube.com/watch?v=iEIqVq7EZqE",
+  },
+  step6: {
+    id: 6,
+    info: "vai ficar tudo bem tudo está no seu devido lugar",
+    confirm: ":)",
+  },
+};
